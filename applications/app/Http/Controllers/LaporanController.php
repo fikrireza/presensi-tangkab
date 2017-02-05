35c1479345786573f7a44bf5ae5c6d5fdcc9826b
fikrireza <fikrirezaa@gmail.com>
2017-02-05 14:04:32 +0700

    Help dud, almost done

--- applications/app/Http/Controllers/LaporanController.php
+++ applications/app/Http/Controllers/LaporanController.php
@@ -45,12 +45,13 @@ class LaporanController extends Controller
                             ->select('preson_pegawais.id as pegawai_id', 'nip_sapk', 'fid', 'tpp_dibayarkan', 'preson_pegawais.nama')->where('skpd_id', $skpd_id)
                             ->orderby('preson_strukturals.nama', 'asc')->get();
 
-      $absensi = DB::select("select a.id, a.fid, nama, tanggal_log, jam_log
-                            from (select id, fid, nama from preson_pegawais where skpd_id = '$skpd_id') as a
+      $absensi = DB::select("select a.id, a.nip_sapk, a.fid, nama, tanggal_log, jam_log, DateTime
+                            from (select id, nip_sapk, fid, nama from preson_pegawais where skpd_id = '$skpd_id') as a
                             left join ta_log b on a.fid = b.fid
                             where str_to_date(b.Tanggal_Log, '%d/%m/%Y') BETWEEN '$start_date' AND '$end_date'
                             AND str_to_date(b.Tanggal_Log, '%d/%m/%Y') NOT IN (SELECT libur FROM preson_harilibur)
-                            AND str_to_date(b.Tanggal_Log, '%d/%m/%Y') NOT IN (SELECT tanggal_mulai FROM preson_intervensis where pegawai_id = a.id and flag_status = 1)");
+                            AND str_to_date(b.Tanggal_Log, '%d/%m/%Y') NOT IN (SELECT tanggal_mulai FROM preson_intervensis where pegawai_id = a.id and flag_status = 1)
+                            ORDER BY DateTime ASC");
       // dd($absensi);
 
       // START = Menghitung Total Datang Terlambat dan Pulang Cepat
@@ -67,31 +68,54 @@ class LaporanController extends Controller
               if ($pegawai->fid == $key->fid) {
                 $jammasuk1 = 80000;
                 $jammasuk2 = 100000;
-                $jamlog = (int) str_replace(':','',$key->jam_log);
-                if( ($jamlog > $jammasuk1) && ($jamlog <= $jammasuk2)){
-                  $jam_masuk[] = $key->fid.'-'.$tanggalini;
-                }
-              }
-            }
-          }
-
-          foreach ($absensi as $key) {
-            if($tanggalini == $key->tanggal_log){
-              if ($pegawai->fid == $key->fid) {
                 $jampulang1 = 140000;
-                $jampulang2 = 160000;
+                $jampulang2 = 155900;
                 $jamlog = (int) str_replace(':','',$key->jam_log);
-                if(($jamlog >= $jampulang1) && ($jamlog < $jampulang2)){
-                  $jam_pulang[] = $key->fid.'-'.$tanggalini;
+                if(($jamlog > $jammasuk1) && ($jamlog <= $jammasuk2)){
+                  $jam_masuk[] = ["fid" => $key->fid, "nip_sapk" => $key->nip_sapk,"tanggal" => $tanggalini, "jam_telat" => $key->jam_log, "jam_pulcep" => ''];
+                }elseif(($jamlog >= $jampulang1) && ($jamlog < $jampulang2)){
+                  $jam_pulang[] = array("fid" => $key->fid, "nip_sapk" => $key->nip_sapk,"tanggal" => $tanggalini, "jam_telat" => '', "jam_pulcep" => $key->jam_log);
                 }
               }
             }
           }
         }
-      }
+      };
 
-      $jam_masuk = array_unique($jam_masuk);
-      $jam_pulang = array_unique($jam_pulang);
+// return $jam_masuk[0]['jam_pulcep'];
+      // $track=0;
+      // foreach ($jam_masuk as $masuk) {
+      //   foreach ($jam_pulang as $pulang) {
+      //     if($masuk['fid'] == $pulang['fid'] && $masuk['tanggal'] == $pulang['tanggal']){
+      //       // $masuk[0]['jam_pulcep'] = $pulang[0]['jam_pulcep'];
+      //       // $masuk[0]['jam_pulcep'] = $pulang[0]['jam_pulcep'];
+      //       $jam_masuk[$track]['jam_pulcep'] = $pulang['jam_pulcep'];
+      //       break;
+      //     }
+      //   }
+      //   $track++;
+      // }
+      $flagpulang=0;
+      foreach ($jam_pulang as $pulang) {
+        $track=0;
+        foreach ($jam_masuk as $masuk) {
+          if($pulang['fid'] == $masuk['fid'] && $pulang['tanggal'] == $masuk['tanggal']){
+            $jam_masuk[$track]['jam_pulcep'] = $pulang['jam_pulcep'];
+            $flagpulang=1;
+            break;
+          }
+          $track++;
+        }
+        if ($flagpulang==0) {
+          $jam_masuk[] = ["fid" => $pulang['fid'], "nip_sapk" => $pulang['nip_sapk'],"tanggal" => $pulang['tanggal'], "jam_telat" => $pulang['jam_telat'], "jam_pulcep" => $pulang['jam_pulcep']];
+        }
+        $flagpulang=0;
+      }
+      return response()->json($jam_masuk);
+//
+//       $array = array_merge($jam_masuk, $jam_pulang);
+// return var_dump($array);
+
 
       if(($jam_masuk==null) && ($jam_pulang==null)){
         $total_telat_dan_pulcep = '';
@@ -137,6 +161,7 @@ class LaporanController extends Controller
                                     AND b.skpd_id = '$skpd_id'
                                     AND str_to_date(a.Tanggal_Log, '%d/%m/%Y') BETWEEN '$start_date' AND '$end_date'
                                     AND TIME_FORMAT(STR_TO_DATE(a.Jam_Log,'%H:%i:%s'), '%H:%i:%s') <= '10:00:00'
+                                    AND str_to_date(a.Tanggal_Log, '%d/%m/%Y') NOT IN (SELECT tanggal_mulai FROM preson_intervensis where pegawai_id = b.id and flag_status = 1)
                                     AND str_to_date(a.Tanggal_Log, '%d/%m/%Y') NOT IN (SELECT libur FROM preson_harilibur)
                                     group By b.id) as tabel_Jumlah_Masuk
                                 ON pegawai.id = tabel_Jumlah_Masuk.pegawai_id");
@@ -145,8 +170,13 @@ class LaporanController extends Controller
 
 
       // START = Get Data Intervensi
-      $intervensi = intervensi::select('pegawai_id', 'tanggal_mulai', 'tanggal_akhir')->whereBetween('tanggal_akhir', array($start_date, $end_date))->where('flag_status', 1)->get();
-      // dd($intervensi);
+      $intervensi = intervensi::join('preson_pegawais', 'preson_pegawais.id', '=', 'preson_intervensis.pegawai_id')
+                								->select('pegawai_id', 'tanggal_mulai', 'tanggal_akhir')
+                								->whereBetween('tanggal_akhir', array($start_date, $end_date))
+                								->where('flag_status', 1)
+                								->where('preson_pegawais.skpd_id', $skpd_id)
+                								->get();
+      //dd($intervensi);
       // END = Get Data Intervensi
 
 
@@ -576,7 +606,7 @@ class LaporanController extends Controller
       // END = Pejabat Dokumen Jika Login sebagai admin skpd
 
       $nama_skpd = skpd::select('nama')->where('id', $skpd_id)->first();
-      
+
       view()->share('start_dateR', $start_dateR);
       view()->share('end_dateR', $end_dateR);
       view()->share('pegawainya', $pegawainya);
@@ -635,15 +665,15 @@ class LaporanController extends Controller
         $list[] = DB::select("SELECT b.id as pegawai_id, a.Tanggal_Log, a.DateTime,
                                 (select MIN(Jam_Log) from ta_log
                                   where DATE_FORMAT(STR_TO_DATE(Tanggal_Log,'%d/%m/%Y'), '%d/%m/%Y') = '$tanggalini'
-                                  and TIME_FORMAT(STR_TO_DATE(Jam_Log,'%H:%i:%s'), '%H:%i:%s') < '10:00:00'
+                                  and TIME_FORMAT(STR_TO_DATE(Jam_Log,'%H:%i:%s'), '%H:%i:%s') < '11:59:00'
                                   and Fid = '$fid->fid'
-                                  and str_to_date(Tanggal_Log, '%d/%m/%Y') NOT IN (SELECT tanggal_mulai FROM preson_intervensis)
+                                  and str_to_date(Tanggal_Log, '%d/%m/%Y') NOT IN (SELECT tanggal_mulai FROM preson_intervensis where pegawai_id = b.id and flag_status = 1)
                                   and str_to_date(Tanggal_Log, '%d/%m/%Y') NOT IN (SELECT libur FROM preson_harilibur)) as Jam_Datang,
                                 (select MIN(Jam_Log) from ta_log
                                   where DATE_FORMAT(STR_TO_DATE(Tanggal_Log,'%d/%m/%Y'), '%d/%m/%Y') = '$tanggalini'
-                                  and TIME_FORMAT(STR_TO_DATE(Jam_Log,'%H:%i:%s'), '%H:%i:%s') > '14:00:00'
+                                  and TIME_FORMAT(STR_TO_DATE(Jam_Log,'%H:%i:%s'), '%H:%i:%s') > '12:01:00'
                                   and Fid = '$fid->fid'
-                                  and str_to_date(Tanggal_Log, '%d/%m/%Y') NOT IN (SELECT tanggal_mulai FROM preson_intervensis)
+                                  and str_to_date(Tanggal_Log, '%d/%m/%Y') NOT IN (SELECT tanggal_mulai FROM preson_intervensis where pegawai_id = b.id and flag_status = 1)
                                   and str_to_date(Tanggal_Log, '%d/%m/%Y') NOT IN (SELECT libur FROM preson_harilibur)) as Jam_Pulang
                               FROM ta_log a, preson_pegawais b, preson_skpd c
                               WHERE b.skpd_id = c.id
@@ -690,13 +720,13 @@ class LaporanController extends Controller
         $list[] = DB::select("SELECT b.id as pegawai_id, a.Tanggal_Log, a.DateTime,
                                 (select MIN(Jam_Log) from ta_log
                                   where DATE_FORMAT(STR_TO_DATE(Tanggal_Log,'%d/%m/%Y'), '%d/%m/%Y') = '$tanggalini'
-                                  and TIME_FORMAT(STR_TO_DATE(Jam_Log,'%H:%i:%s'), '%H:%i:%s') < '10:00:00'
+                                  and TIME_FORMAT(STR_TO_DATE(Jam_Log,'%H:%i:%s'), '%H:%i:%s') < '11:59:00'
                                   and Fid = '$fid->fid'
                                   and str_to_date(Tanggal_Log, '%d/%m/%Y') NOT IN (SELECT tanggal_mulai FROM preson_intervensis where pegawai_id = b.id and flag_status = 1)
                                   and str_to_date(Tanggal_Log, '%d/%m/%Y') NOT IN (SELECT libur FROM preson_harilibur)) as Jam_Datang,
                                 (select MIN(Jam_Log) from ta_log
                                   where DATE_FORMAT(STR_TO_DATE(Tanggal_Log,'%d/%m/%Y'), '%d/%m/%Y') = '$tanggalini'
-                                  and TIME_FORMAT(STR_TO_DATE(Jam_Log,'%H:%i:%s'), '%H:%i:%s') > '14:00:00'
+                                  and TIME_FORMAT(STR_TO_DATE(Jam_Log,'%H:%i:%s'), '%H:%i:%s') > '12:01:00'
                                   and Fid = '$fid->fid'
                                   and str_to_date(Tanggal_Log, '%d/%m/%Y') NOT IN (SELECT tanggal_mulai FROM preson_intervensis where pegawai_id = b.id and flag_status = 1)
                                   and str_to_date(Tanggal_Log, '%d/%m/%Y') NOT IN (SELECT libur FROM preson_harilibur)) as Jam_Pulang
